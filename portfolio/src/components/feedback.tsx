2e7d9a2f10792b56bb26397f58c1a4b2d4e7eef2
EduardoMenen <Eduardoriboli12@hotmail.com>
2025-03-18 10:22:02 -0300

    Card de FeedBack

--- portfolio/src/components/feedback.tsx
+++ portfolio/src/components/feedback.tsx
@@ -6,7 +6,18 @@ export default function Feedback() {
             <div>
                 <h2 className="TituloFeedback">O que nossos clientes dizem sobre nós</h2>
             </div>
-            
+            <div className="ContainerFeedback">
+                <div className="TituloContainer">
+                    <img src="Eduardo.jpg" alt="Foto Do Cliente" />
+                    <h3>Alã Menegazzo</h3>
+                    <p>Alã Automóveis, Não-me-Toque</p>
+                </div>
+                <div className="Depoimento">
+                    <p>"Profissionais excelentes! Desenvolveram nosso site <br />
+                        exatamente como queríamos, com um design moderno e fácil de usar. <br />
+                        O suporte também foi incrível. Recomendo!"</p>
+                </div>
+            </div>
         </div>
     )
 }
